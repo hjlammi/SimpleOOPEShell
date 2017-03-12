474de9196de9080e03e7f7f2f40b93dcbf7ff523
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2017-03-12 17:40:59 +0200

    Add hae method

--- src/main/java/oope/OmaLista.java
+++ src/main/java/oope/OmaLista.java
@@ -6,19 +6,29 @@ import fi.uta.csjola.oope.lista.LinkitettyLista;
 
 public class OmaLista extends LinkitettyLista implements Ooperoiva {
 
-	public Object hae(Object haettava) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Object hae(Object haettava) {
+        if (haettava == null) {
+            return null;
+        } else if (onkoTyhja()) {
+            return null;
+        } else {
+            for (int i = 0; i < koko(); i++) {
+                if (haettava.equals(alkio(i))) {
+                    return alkio(i);
+                }
+            }
+            return null;
+        }
+    }
 
-	public boolean lisaa(Object uusi) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public boolean lisaa(Object uusi) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	public Object poista(Object poistettava) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Object poista(Object poistettava) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
--- src/test/java/oope/OmaListaTest.java
+++ src/test/java/oope/OmaListaTest.java
@@ -1,21 +1,86 @@
 package oope;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class OmaListaTest {
 
-	@Test
-	public void tyhja() {
-		OmaLista lista = new OmaLista();
-		assertEquals(true, lista.onkoTyhja());
-	}
+    @Test
+    public void tyhja() {
+        OmaLista lista = new OmaLista();
+        assertEquals(true, lista.onkoTyhja());
+    }
 
-	@Test
-	public void yksi() {
-		OmaLista lista = new OmaLista();
-		String alkio = "abc";
-		lista.lisaaAlkuun(alkio);
-		assertEquals(false, lista.onkoTyhja());
-	}
+    @Test
+    public void yksi() {
+        OmaLista lista = new OmaLista();
+        lista.lisaaAlkuun("abc");
+        assertEquals(false, lista.onkoTyhja());
+    }
+
+    @Test
+    public void tyhjastaHaku() {
+        OmaLista lista = new OmaLista();
+        assertEquals(null, lista.hae("ab"));
+    }
+
+    @Test
+    public void haku1VaaraAlkio() {
+        OmaLista lista = new OmaLista();
+        lista.lisaaAlkuun("abc");
+        assertEquals(null, lista.hae("ab"));
+    }
+
+    @Test
+    public void haku1OikeaAlkio() {
+        OmaLista lista = new OmaLista();
+        lista.lisaaAlkuun("ab");
+        assertEquals("ab", lista.hae("ab"));
+    }
+
+    @Test
+    public void haku1OikeaAlkioKolmesta() {
+        OmaLista lista = new OmaLista();
+        lista.lisaaAlkuun("Ab");
+        lista.lisaaAlkuun("ab");
+        lista.lisaaAlkuun("AB");
+        assertEquals("ab", lista.hae("ab"));
+    }
+
+    @Test
+    public void haettuaAlkiotaEiOleListassa() {
+        OmaLista lista = new OmaLista();
+        lista.lisaaAlkuun("Ab");
+        lista.lisaaAlkuun("bb");
+        lista.lisaaAlkuun("AB");
+        assertEquals(null, lista.hae("ab"));
+    }
+
+    @Test
+    public void nullillaHaku() {
+        OmaLista lista = new OmaLista();
+        lista.lisaaAlkuun("Ab");
+        lista.lisaaAlkuun("bb");
+        lista.lisaaAlkuun("AB");
+        assertEquals(null, lista.hae(null));
+    }
+
+    @Test
+    public void nullillaHakuyksiNull() {
+        OmaLista lista = new OmaLista();
+        lista.lisaaAlkuun("Ab");
+        lista.lisaaAlkuun(null);
+        lista.lisaaAlkuun("AB");
+        assertEquals(null, lista.hae(null));
+    }
+
+    @Test
+    public void listassaNull() {
+        OmaLista lista = new OmaLista();
+        lista.lisaaAlkuun("Ab");
+        lista.lisaaAlkuun(null);
+        lista.lisaaAlkuun("AB");
+        assertEquals("AB", lista.hae("AB"));
+    }
 }
