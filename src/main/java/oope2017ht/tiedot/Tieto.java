3620b3bc27d70897297db587459f4791183184d3
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2017-03-26 17:08:40 +0300

    Override equals method

--- src/main/java/oope2017ht/tiedot/Tieto.java
+++ src/main/java/oope2017ht/tiedot/Tieto.java
@@ -101,4 +101,11 @@ public abstract class Tieto implements Comparable<Tieto>{
         }
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        String tama = this.nimi().toString();
+        String toinen = ((Tieto)obj).nimi().toString();
+        return tama.equals(toinen);
+    }
+
 }
