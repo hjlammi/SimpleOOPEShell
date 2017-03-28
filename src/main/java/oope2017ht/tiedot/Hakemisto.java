d4b20ed75daf6cafbbc279134f8df828602c5741
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2017-03-28 17:43:25 +0300

    Add comments to Hakemisto methods

--- src/main/java/oope2017ht/tiedot/Hakemisto.java
+++ src/main/java/oope2017ht/tiedot/Hakemisto.java
@@ -42,6 +42,7 @@ public class Hakemisto extends Tieto implements Komennettava<Tieto> {
     }
 
     // Korvattava toString-metodi.
+    @Override
     public String toString() {
         return super.toString() + "/" + EROTIN + tiedot.koko();
     }
@@ -50,42 +51,42 @@ public class Hakemisto extends Tieto implements Komennettava<Tieto> {
      * Komennettava-rajapinnan korvattavat metodit.
      */
 
-    /* Aksessori, joka antaa viitteen hakemiston sisällön säilövään listaan.
-    *
-    * @return viite hakemisto-olion osaolioon.
-    */
+    /*
+     * Aksessori, joka antaa viitteen hakemiston sisällön säilövään listaan.
+     */
     @Override
     public LinkitettyLista sisalto() {
         return tiedot;
     }
 
-
-    /* Hakee hakemistosta tiedostoa tai alihakemistoa. Hyödyntää OmaLista-luokan
-    * hae-operaatiota. Huomaa, että nimeä käyttäen haun avuksi voidaan luoda
-    * väliaikainen tiedosto tai hakemisto.
-    *
-    * @param nimi haettavan tiedon nimi.
-    * @return viite löydettyyn tietoon. Paluuarvo on null, jos tietoa ei löydetä.
-    */
+    /* Hakee hakemistosta parametrina saatua nimeä vastaavaa tiedostoa tai alihakemistoa.
+     * Hyödyntää OmaLista-luokan hae-operaatiota. Paluuarvo on viite löydettyyn tietoon tai null,
+     * jos tietoa ei löydetä.
+     */
     @Override
     public Tieto hae(String nimi) {
+     // Luodaan apuolio, jolle annetaan nimeksi parametrina saatu nimi.
         Tieto haettava = new Tiedosto(new StringBuilder(nimi), 1);
+        // Käytetään apuna OmaLista-luokan hae-metodia, joka hakee hakemistosta
+        // parametrina saatua nimeä vastaavaa tietoa.
+        // Paluuarvona saadaan viite haettavaa vastaavaan tietoon tai null, jos tietoa ei löydy.
         Object loydetty = tiedot.hae(haettava);
         return (Tieto)loydetty;
     }
 
-    /* Lisää hakemistoon tiedoston tai alihakemiston. Hyödyntää OmaLista-luokan
-     * lisaa-operaatiota.
-     *
-     * @param lisattava viite lisättävään tietoon.
-     * @return true, jos lisääminen onnistui ja false, jos tieto on null-arvoinen
+    /* Lisää hakemistoon parametrina annetun tiedoston tai alihakemiston. Hyödyntää OmaLista-luokan
+     * lisaa-operaatiota. Paluuarvo on true, jos lisääminen onnistui ja false, jos tieto on null-arvoinen
      * tai hakemistossa on jo tieto parametrina annetulla nimellä.
      */
     @Override
     public boolean lisaa(Tieto lisattava) {
+        // Tarkistetaan, ettei lisättävä ole null-arvoinen.
         if (lisattava != null) {
+            // Käydään hakemiston sisältö läpi ja verrataan lisättävää listassa jo oleviin tietoihin.
             for (int i = 0; i < tiedot.koko(); i++) {
                 Object alkio = tiedot.alkio(i);
+                // Jos hakemistosta löytyy jo samanniminen tieto, palautetaan false. Muuten lisätään
+                // lisättävä tieto OmaLista-luokan lisaa-metodilla ja palautetaan true.
                 if (lisattava.equals((Tieto)alkio)) {
                     return false;
                 }
@@ -97,18 +98,19 @@ public class Hakemisto extends Tieto implements Komennettava<Tieto> {
         }
     }
 
-    /* Poistaa hakemistosta tiedoston tai alihakemiston. Hyödyntää OmaLista-luokan
-     * poista-operaatiota. Huomaa, että nimeä käyttäen poiston avuksi voidaan luoda
-     * väliaikainen tiedosto tai hakemisto.
-     *
-     * @param nimi poistettavan tiedon nimi.
-     * @return viite poistettuun tietoon. Paluuarvo on null, jos tietoa ei löydetä.
+    /* Poistaa hakemistosta parametrina saatua nimeä vastaavan tiedoston tai alihakemiston.
+     * Hyödyntää OmaLista-luokan poista-operaatiota. Paluuarvo on viite poistettuun tietoon tai
+     * null, jos tietoa ei löydetä.
      */
     @Override
     public Tieto poista(String nimi) {
+        // Luodaan apuolio, jolle annetaan nimeksi parametrina saatu nimi.
         Tieto poistettava = new Tiedosto(new StringBuilder(nimi), 1);
+        // Käytetään apuna OmaLista-luokan poista-metodia, joka poistaa hakemistosta
+        // poistettavan nimisen tiedoston tai alihakemiston ja antaa viitteen siihen paluuarvona.
+        // Paluuarvo on null, jos nimeä vastaavaa tietoa ei löydy.
         Object loydetty = tiedot.poista(poistettava);
+        // Muutetaan saatu tieto Tieto-tyyppiseksi ja annetaan se paluuarvona.
         return (Tieto)loydetty;
     }
-
 }
