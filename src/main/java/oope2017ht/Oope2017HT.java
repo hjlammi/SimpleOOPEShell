e4b5d7d6f9c89065f1983ee390ff35789131af26
Heidi Lammi-Mihaljov <heidi.lammi@mihaljov.info>
2017-04-23 12:50:34 +0300

    Add general comments for Oope2017HT

--- src/main/java/oope2017ht/Oope2017HT.java
+++ src/main/java/oope2017ht/Oope2017HT.java
@@ -5,17 +5,20 @@ package oope2017ht;
 *
 * Heidi Lammi-Mihaljov, Lammi-Mihaljov.Heidi.J@student.uta.fi.
 *
-* Viimeksi muokattu 12.4.2017.
+* Viimeksi muokattu 23.4.2017.
 *
-*
+* Ajoluokan tehtävänä on luoda käyttöliittymä ja kutsua sen suorita-metodia.
 */
 
 public class Oope2017HT
 {
     public static void main(String[] args) {
         Terminaali terminaali = new StdTerminaali();
+
+        // Luodaan Kayttoliittyma-olio.
         Kayttoliittyma kayttoliittyma = new Kayttoliittyma(terminaali);
 
+        // Kutsutaan olion suorita-metodia.
         kayttoliittyma.suorita();
     }
 }
